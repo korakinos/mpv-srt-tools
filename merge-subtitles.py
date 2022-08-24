d412499a18b875221d78811a2100ae2181fdcb9d
Carlos Franke <>
2022-08-24 21:07:10 +0200

    fixed syntax error in python script
    Did this use to work? With which version of Python? The code looked much nicer before, with the line break after the comma.

--- merge-subtitles.py
+++ merge-subtitles.py
@@ -9,7 +9,7 @@ with open("subtitles.txt") as text_file:
     for line in text_file:
         if line.isspace():
             # save finished paragraph, if not empty
-            if (paragraph and not paragraph.isspace()):
+            if paragraph and not paragraph.isspace():
                 paragraphs.append(paragraph)
             paragraph = ""
         else:
@@ -18,14 +18,13 @@ with open("subtitles.txt") as text_file:
 
 # backup any previously generated file
 try:
-    os.rename(
-        "subtitles.srt",
-        "subtitles-backup-" + strftime("%FT%T") + ".srt")
+    os.rename("subtitles.srt", "subtitles-backup-" + strftime("%FT%T") + ".srt")
 except FileNotFoundError:
     pass
 
 # merge SRT template with text paragraphs, write SRT file
-with open("subtitles-template.srt") as template_file,
-with open("subtitles.srt", "w") as merged_file:
+with open("subtitles-template.srt") as template_file, open(
+    "subtitles.srt", "w"
+) as merged_file:
     srt_data = template_file.read()
     merged_file.write(srt_data.format(*paragraphs))
